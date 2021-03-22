2340a912799225cfc71cab005444e57948ff994c
Asus <braiimauricio@hotmail.com>
2021-03-22 16:35:01 -0500

    webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 <!-- Script to change bg of navbar on scroll-->
     $(window).scroll(function (){
-    $('nav').toggleClass('scrolled',$(this).scrollTop() > 20);
+    $('nav').toggleClass('scrolled',$(this).scrollTop() > 50);
 })
